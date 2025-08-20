2f7b36c6e846637a23c0980b7d77edcec9b967af
Iszreen17 <140799200+Iszreen17@users.noreply.github.com>
2025-08-20 14:10:49 +0800

    changed the teddy script

--- Assets/Scripts/TeddyBearCollect.cs
+++ Assets/Scripts/TeddyBearCollect.cs
@@ -2,17 +2,12 @@ using UnityEngine;
 using TMPro;
 public class TeddyBearCollect : MonoBehaviour
 {
-    public TextMeshProUGUI congratsText;
+    
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Teddy collected!");
-            if (congratsText != null)
-            {
-                congratsText.text = "Congratulations! You collected the Teddy Bear!";
-                congratsText.gameObject.SetActive(true);
-            }
             Destroy(gameObject);
         }
     }
